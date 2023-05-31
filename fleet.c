78c63fc0d77da9ba62bb218649bad67b5c4638d7
Joycewutsika <joycesena86@gmail.com>
2023-05-31 08:00:04 -0700

    fleet.c

--- fleet.c
+++ fleet.c
@@ -16,11 +16,11 @@ int process_print(const char *detail, int *i, va_list joy, char buffer[],
 {
 	int x, len1 = 0, output_chars = -1;
 	detail_t detail_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_Hex_up}, {'p', print_pointer}, {'S', display_non_print_char},
-		{'r', print_rever}, {'R', print_rot13}, {'\0', NULL}
+		{"c", print_char}, {"s", print_string}, {"%", print_percent},
+		{"i", print_int}, {"d", print_int}, {"b", print_binary},
+		{"u", print_unsigned}, {"o", print_octal}, {"x", print_hexadecimal},
+		{"X", print_Hex_up}, {"p", print_pointer}, {"S", display_non_print_char},
+		{"r", print_rever}, {"R", print_rot13}, {'\0', NULL}
 	};
 	for (x = 0; detail_types[x].detail != '\0'; x++)
 		if (detail[*i] == detail_types[x].detail)
