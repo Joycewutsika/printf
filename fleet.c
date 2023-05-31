e270fc6e1a592962999afc15a968360e60b39c09
SandraBoadi <boadisandra@yahoo.com>
2023-05-30 19:08:19 -0700

    fleet.c

--- fleet.c
+++ fleet.c
@@ -15,18 +15,18 @@ int process_print(const char *detail, int *ind, va_list joy, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int x, len1 = 0, output_chars = -1;
-	detail_t detail_joy[] = {
+	detail_t detail_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_Hex_up}, {'p', print_pointer}, {'S', display_non_print_char},
 		{'r', print_rever}, {'R', print_rot13}, {'\0', NULL}
 	};
-	for (x = 0; detail_joy[x].detail != '\0'; x++)
-		if (detail[*ind] == detail_joy[x].detail)
-			return (detail_joy[x].fxn(joy, buffer, flags, width, precision, size));
+	for (x = 0; detail_types[x].detail != '\0'; x++)
+		if (detail[*ind] == detail_types[x].detail)
+			return (detail_types[x].fxn(joy, buffer, flags, width, precision, size));
 
-	if (detail_joy[x].detail == '\0')
+	if (detail_types[x].detail == '\0')
 	{
 		if (detail[*ind] == '\0')
 			return (-1);
