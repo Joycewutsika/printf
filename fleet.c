51ae6dc04f9ef983f142ca69b3980c5ad7588f13
Joycewutsika <joycesena86@gmail.com>
2023-05-31 05:56:03 -0700

    fleet.c

--- fleet.c
+++ fleet.c
@@ -1,61 +1,16 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
- * process_print - Prints an argument based on its type
- * @sym: format string
- * @ind: ind.
- * @joy: arguments list
- * @buffer: Buffer
-=======
  * process_print - based on its type it prints an argument
  * @detail: string format specifier
  * @joy: List of arguments
  * @i: ind.
  * @buffer: Buffer array
->>>>>>> 0ee2a8944e5b8f89646304ec5ef8bf7a0991eabe
  * @flags: flags
- * @width: width.
- * @precision: Precision specification
- * @size: Size specifier
+ * @width: width
+ * @precision: Precision
+ * @size: Size
  * Return: 1 or 2;
  */
-<<<<<<< HEAD
-int process_print(const char *sym, int *ind, va_list joy, char buffer[],
-        int flags, int width, int precision, int size)
-{
-        int x, len1 = 0, output_chars = -1;
-        sym_t sym_types[] = {
-                {'c', print_char}, {'s', print_string}, {'%', print_percent},
-                {'i', print_int}, {'d', print_int}, {'b', print_binary},
-                {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-                {'X', print_Hex_up}, {'p', print_pointer}, {'S', display_non_print_char},
-                {'r', print_rever}, {'R', print_rot13}, {'\0', NULL}
-        };
-        for (x = 0; sym_types[x].sym != '\0'; x++)
-                if (sym[*ind] == sym_types[x].sym)
-                        return (sym_types[x].fxn(joy, buffer, flags, width, precision, size));
-
-        if (sym[*ind] == '\0')
-        {
-                if (sym[*ind] == '\0')
-                        return (-1);
-                len1 += write(1, "%%", 1);
-                if (sym[*ind - 1] == ' ')
-                        len1 += write(1, " ", 1);
-                else if (width)
-                {
-                        --(*ind);
-                        while (sym[*ind] != ' ' && sym[*ind] != '%')
-                                --(*ind);
-                        if (sym[*ind] == ' ')
-                                --(*ind);
-                        return (1);
-                }
-                len1 += write(1, &sym[*ind], 1);
-                return (len1);
-        }
-        return (output_chars);
-=======
 int process_print(const char *detail, int *i, va_list joy, char buffer[],
 		int flags, int width, int precision, int size)
 {
@@ -91,6 +46,4 @@ int process_print(const char *detail, int *i, va_list joy, char buffer[],
 		return (len1);
 	}
 	return (output_chars);
->>>>>>> 0ee2a8944e5b8f89646304ec5ef8bf7a0991eabe
 }
-
